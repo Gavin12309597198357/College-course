65abb52b36b875405063b8515b851de1b856693b
Matthew Snader <47010772+MatthewMonkey1@users.noreply.github.com>
2019-03-12 13:27:58 -0500

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student
+{
+    CollegeCourse[] courses = new CollegeCourse[5];
+    private int sNum;
+
+    Student(){
+        for (int i=0; i<courses.length; i++){
+            this.courses[i]=new CollegeCourse();}
+    }
+
+    public void setCourseID(String id, int index) {this.courses[index].setCourseID(id);}
+    public CollegeCourse getCourse(int index) { return this.courses[index];}
+    public void setStudentNum(int stu) {this.sNum=stu;}
+    public int getStudentNum() {return this.sNum;}
+}
